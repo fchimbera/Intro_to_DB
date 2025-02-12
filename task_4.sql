f6cd9e6739ba45a2287f74e19edde8e2220138d8
fchimbera <chimberafreeman@gmail.com>
2025-02-12 23:53:40 +0200

    update task_4

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Use the database passed as an argument
+USE alx_book_store;
+
+-- Print the full description of the books table
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
